d449e36dbc8acfe07a7f61fe3e09941b91372bb8
Ermolvi4 <ermolovichrenat@gmail.com>
2025-05-11 15:05:50 +1000

    Relis the programm The ball

--- Bubs/Bubs/Bubs.h
+++ Bubs/Bubs/Bubs.h
@@ -3,9 +3,6 @@
 //Geting values from users
 int Get_value_from_user();
 
-//The calculations of the lenght how nuch fell to ball and the printim value
-void Calculates_len_fell(int hight_tower);
-
 //The forms for calculates
 int Form_acceleration_of_the_fell(int hight_tower);
 #endif
--- Bubs/Bubs/MathBubs.cpp
+++ Bubs/Bubs/MathBubs.cpp
@@ -20,30 +20,6 @@ int Form_acceleration_of_the_fell(int second)
 	return hight_fell;
 }
 
-void Calculates_len_fell(int hight_tower)
-{
-	//calculats fall for x_second
-	std::cout << "From 0 second fell";
-	std::cout << Form_acceleration_of_the_fell(0) << std::endl;
-	
-	/
-	std::cout << "From 1 second fell";
-	std::cout << Form_acceleration_of_the_fell(1) << std::endl;
-	
-	std::cout << "From 2 second fell";
-	std::cout << Form_acceleration_of_the_fell(2) << std::endl;
-	
-	std::cout << "From 3 second fell";
-	std::cout << Form_acceleration_of_the_fell(3) << std::endl;
-	
-	std::cout << "From 4 second fell";
-	std::cout << Form_acceleration_of_the_fell(4) << std::endl;
-	
-	std::cout << "From 5 second fell";
-	std::cout << Form_acceleration_of_the_fell(5) << std::endl;
-	
-}
-
 
 
 
